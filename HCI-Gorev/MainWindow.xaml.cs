42a6393417182e3809d5d76ef66c6de0b7d54b38
mustafaberat <mustafa.ceren.18@hotmail.com>
2020-11-02 12:40:30 +0300

    init

---
+++ HCI-Gorev/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HCI_Gorev
+{
+    /// <summary>
+    /// MainWindow.xaml etkileşim mantığı
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void baslabtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(textBoxName.Text.Trim() != "")
+            {
+                Test1 test1 = new Test1(textBoxName.Text);
+                test1.Show();
+                test1.showName();
+                this.Close(); 
+            }
+        }
+    }
+}
---
+++ HCI-Gorev/Test1.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HCI_Gorev
+{
+    /// <summary>
+    /// test1.xaml etkileşim mantığı
+    /// </summary>
+    public partial class Test1: Window
+    {
+        private string Isim { get; set; }
+        public Test1(string isim = "NULL")
+        {
+            InitializeComponent();
+            Isim = isim;
+        }
+
+        public void showName()
+        {
+            MessageBox.Show(Isim, "My App");
+        }
+
+    }
+}
